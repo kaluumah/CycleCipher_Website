1d1f9812dbdbb531c8035fcc771da23f45cd4d90
kaluumah <winnerkalu1303@gmail.com>
2025-02-17 23:51:32 +0100

    Docs: Updated Settings
    I updated the database settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cyclecipherdatabase',
+        'USER': 'cyclecipher',
+        'PASSWORD': 'cycl3c1ph3r',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
