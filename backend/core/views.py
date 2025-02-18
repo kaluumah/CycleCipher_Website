88b4b8eabdd8c1410a0c7141b5003716bc4c2342
kaluumah <winnerkalu1303@gmail.com>
2025-02-18 01:23:27 +0100

    Docs: Completed the Backend Configuration

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .models import Waitlist
+from .serializers import WaitlistSerializer
 
 # Create your views here.
+class WaitlistCreateView(CreateAPIView):
+    queryset = Waitlist.objects.all()
+    serializer_class = WaitlistSerializer
